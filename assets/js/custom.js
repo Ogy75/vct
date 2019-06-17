4553c1c019b5a56ebc4670acb7413329c36bb689
Oggy <ognjen.djokic@gmail.com>
2019-06-17 12:05:23 +0200

    spinner fix

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,4 +1,4 @@
-$(function() {
+$(function () {
 
     //vacation page log height fix
     $('.height-fix').height($('.vct-days__remaining').height());
@@ -13,7 +13,7 @@ $(function() {
         menuPanel.addClass('hide-left');
         contentExpand.addClass('expanded').addClass('vct-content');
     }
-    menuToggleButton.on('click', function() {
+    menuToggleButton.on('click', function () {
         menuPanel.toggleClass('hide-left');
         contentExpand.toggleClass('expanded');
         if (windowWidth < 1281) {
@@ -24,7 +24,7 @@ $(function() {
     //left side menu toggle navigation
     var subMenuButton = $('a.has-level1');
     var subMenuItem = $('.vct-nav-level1');
-    subMenuButton.on('click', function(ev) {
+    subMenuButton.on('click', function (ev) {
         var trigger = $(ev.target);
         trigger.toggleClass('btn-highlight');
         trigger.next(subMenuItem).slideToggle(200);
@@ -35,10 +35,10 @@ $(function() {
     var profile = $('.vct-user-profile-image img');
     var profileMenu = $('.vct-user-menu-block');
 
-    profile.on('click', function() {
+    profile.on('click', function () {
         profileMenu.fadeToggle(100);
     });
-    $(document).click(function(event) {
+    $(document).click(function (event) {
         var target = $(event.target);
         if (!target.closest(profile).length &&
             $(profileMenu).is(":visible")) {
@@ -47,18 +47,18 @@ $(function() {
     });
 
     //fullscreen function
-    $(".full-view").on('click', function() {
+    $(".full-view").on('click', function () {
         document.fullScreenElement && null !== document.fullScreenElement || !document.mozFullScreen && !document.webkitIsFullScreen ? document.documentElement.requestFullScreen ? document.documentElement.requestFullScreen() : document.documentElement.mozRequestFullScreen ? document.documentElement.mozRequestFullScreen() : document.documentElement.webkitRequestFullScreen && document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT) : document.cancelFullScreen ? document.cancelFullScreen() : document.mozCancelFullScreen ? document.mozCancelFullScreen() : document.webkitCancelFullScreen && document.webkitCancelFullScreen()
     });
 
     //deviation value
-    $('.deviation').each(function(i, e) {
+    $('.deviation').each(function (i, e) {
         var el = $(e);
         el.addClass(parseInt(el.val()) < 0 ? 'negative-value' : '');
     });
 
     //scroll hide
-    $(window).scroll(function() {
+    $(window).scroll(function () {
         if ($(window).scrollTop() > 200) {
             $('.vct-top-nav, .vct-left-nav-toggle').addClass('nav-up');
         }
@@ -73,19 +73,19 @@ $(function() {
     var totalPanel = $('.vct-total-toggle-wrapper');
     var totalPanelHeight = totalPanel.height();
 
-    card.on('click', function() {
+    card.on('click', function () {
         totalPanel.slideToggle(100);
         card.find('i').toggleClass('ti-angle-up');
     });
 
     //tooltip ini
-    $(function() {
+    $(function () {
         $('[data-toggle="tooltip"]').tooltip();
     });
 
     //notification badge demo /spinner
-    setTimeout(function() {
-        $('.alert').show().delay(1500).queue(function(n) {
+    setTimeout(function () {
+        $('.alert').show().delay(1500).queue(function (n) {
             $('.lds-dual-ring-bg').fadeOut(200);
             $(this).hide();
         });
@@ -94,19 +94,19 @@ $(function() {
 
 
     //back btn
-    $('.vct-back').on('click', function() {
+    $('.vct-back').on('click', function () {
         window.history.back();
     });
 
     //add team member - time
     var teamMember = '<div class="py-3 vct-team-member-block"> <div class="row mb-2"> <div class="col-5 vct-team-member-name font-weight-normal"> <label for="employees">employee name</label> <input class="w-100" id="employees" placeholder="enter name" value="" /> </div> <div class="col-5 vct-team-member-name font-weight-normal"> <label for="employees">position</label> <input class="w-100" id="position" placeholder="find position" value="" /> </div> <div class="col-2"> <div class="d-flex align-items-start justify-content-end h-100"> <button class="btn ti-close btn-action remove-team-member p-0" data-toggle="tooltip" data-placement="top" title="Remove Team Member"></button> </div> </div> </div> <div class="row"> <div class="col-12"> <div class="row"> <div class="col-12 col-lg-2 align-self-end mt-3"> <div class="d-flex align-items-center vct-team-location"> <p class="w-100 text-center">Belgrade</p> </div> </div> <div class="col-3 col-lg-1 position-relative"> <label for="normal" data-toggle="tooltip" data-placement="top" title="Normal - vreme provedeno na komercijalnim projektima">Normal</label> <input type="text" value="0" name="normal"> </div> <div class="col-3 col-lg-1"> <label for="travel" data-toggle="tooltip" data-placement="top" title="Travel - vreme provedeno u putu prilikom odlaska kod klijenta">Travel</label> <input type="text" value="0" name="travel"> </div> <div class="col-3 col-lg-1"> <label for="totalWork">Total work</label> <input type="text" value="0" name="totalWork"> </div> <div class="col-3 col-lg-1"> <label for="overtime" data-toggle="tooltip" data-placement="top" title="Overtime - rad na projektima radnim danima posle 19h i vikendima!">Overtime</label> <input type="text" value="0" name="overtime"> </div> <div class="col-3 col-lg-1"> <label for="eiInternal" data-toggle="tooltip" data-placement="top" title="Ei Internal - rad na internim projektima ili overhead koji je nastao usled administrativnih obaveza (razni sastanci, godisnji razgovori, itd.)">Ei internal</label> <input type="text" value="0" name="eiInternal"> </div> <div class="col-3 col-lg-1"> <label for="free" data-toggle="tooltip" data-placement="top" title="Free - fond sati koji dopunjuje ukupne mesecne sate - koristimo ga za nove kolege koji dodju u sred meseca pa time dopunimo vreme (npr. ako je neko dosao 15-og u mesecu posle 10 radnih dana u Free ce biti upisano 80h), takodje ga koristimo za kolege koji odlaze iz firme po slicnom principu).">Free</label> <input type="text" value="0" name="free"> </div> <div class="col-3 col-lg-1"> <label for="holidays" data-toggle="tooltip" data-placement="top" title="Holiday - vreme / dani koji se oduzimaju od fonda godisnjeg odmora">Holidays</label> <input type="text" value="0" name="holidays"> <i class="info-modal-trigger ti-more-alt position-absolute" data-toggle="modal" data-target="#holidays"></i> </div> <div class="col-3 col-lg-1"> <label for="sickness" data-toggle="tooltip" data-placement="top" title="Sickness - bolovanja">Sickness</label> <input type="text" value="0" name="sickness"> </div> <div class="col-3 col-lg-1"> <label for="daysOff" data-toggle="tooltip" data-placement="top" title="Days off - slobodni dani na ime zakonskog prava (slava, vencanje, rodjenje deteta,...)">Days Off</label> <input type="text" value="0" name="daysOff"> </div> <div class="col-3 col-lg-1"> <label for="total">Total</label> <input type="text" value="0" name="total" readonly> </div> </div> <div class="row"> <div class="col-12 col-lg-2 align-self-end mt-3"> <div class="d-flex align-items-center vct-team-location"> <p class="w-100 text-center">On-site</p> </div> </div> <div class="col-3 col-lg-1"> <label for="normal" data-toggle="tooltip" data-placement="top" title="Normal - vreme provedeno na komercijalnim projektima">Normal</label> <input type="text" value="0" name="normal"> </div> <div class="col-3 col-lg-1"> <label for="travel" data-toggle="tooltip" data-placement="top" title="Travel - vreme provedeno u putu prilikom odlaska kod klijenta">Travel</label> <input type="text" value="0" name="travel"> </div> <div class="col-3 col-lg-1"> <label for="totalWork">Total work</label> <input type="text" value="0" name="totalWork"> </div> <div class="col-3 col-lg-1"> <label for="overtime" data-toggle="tooltip" data-placement="top" title="Overtime - rad na projektima radnim danima posle 19h i vikendima!">Overtime</label> <input type="text" value="0" name="overtime"> </div> <div class="col-3 col-lg-1"> <label for="eiInternal" data-toggle="tooltip" data-placement="top" title="Ei Internal - rad na internim projektima ili overhead koji je nastao usled administrativnih obaveza (razni sastanci, godisnji razgovori, itd.)">Ei internal</label> <input type="text" value="0" name="eiInternal"> </div> <div class="col-3 col-lg-1"> <label for="free" data-toggle="tooltip" data-placement="top" title="Free - fond sati koji dopunjuje ukupne mesecne sate - koristimo ga za nove kolege koji dodju u sred meseca pa time dopunimo vreme (npr. ako je neko dosao 15-og u mesecu posle 10 radnih dana u Free ce biti upisano 80h), takodje ga koristimo za kolege koji odlaze iz firme po slicnom principu).">Free</label> <input type="text" value="0" name="free"> </div> <div class="col-3 col-lg-1"> <label for="holidays" data-toggle="tooltip" data-placement="top" title="Holiday - vreme / dani koji se oduzimaju od fonda godisnjeg odmora">Holidays</label> <input type="text" value="0" name="holidays"> </div> <div class="col-3 col-lg-1"> <label for="sickness" data-toggle="tooltip" data-placement="top" title="Sickness - bolovanja">Sickness</label> <input type="text" value="0" name="sickness"> </div> <div class="col-3 col-lg-1"> <label for="daysOff" data-toggle="tooltip" data-placement="top" title="Days off - slobodni dani na ime zakonskog prava (slava, vencanje, rodjenje deteta,...)">Days Off</label> <input type="text" value="0" name="daysOff"> </div> <div class="col-3 col-lg-1"> <label for="total">Total</label> <input type="text" value="0" name="total" readonly> </div> </div> </div> <div class="col-3 col-lg-1"> <label for="deviation" data-toggle="tooltip" data-placement="top" title="Odsutupanje od potrebnog vremena">Deviation</label> <input class="deviation" type="number" value="0" name="deviation" readonly> </div> </div> <div class="row align-items-end mb-3"> <div class="col-12 col-md-7"> <label for="notes">Notes</label> <textarea name="notes" rows="3"></textarea> </div> <div class="col-12 col-md-5 mt-2 text-left text-md-right"> <button type="button" class="btn btn-highlight px-4">Save</button> </div> </div> </div>';
 
-    $(document).on('click', '.add-team-member', function() {
+    $(document).on('click', '.add-team-member', function () {
         $('.team-members').append(teamMember);
     });
-    $(document).on('click', '.remove-team-member', function() {
+    $(document).on('click', '.remove-team-member', function () {
         $(this).closest($('.vct-team-member-block')).slideUp(200);
-        setTimeout(function() {
+        setTimeout(function () {
             $(this).closest($('.vct-team-member-block')).remove();
         }, 150);
     });
@@ -114,12 +114,12 @@ $(function() {
     //add team member - team
     var teamMember1 = '<div class="py-3 vct-team-member-block"> <div class="row mb-3"> <div class="col-5 vct-team-member-name font-weight-normal"> <label for="employees">employee name</label> <input class="w-100" id="employees" placeholder="enter name" /> </div> <div class="col-5 vct-team-member-name font-weight-normal"> <label for="employees">position</label> <input class="w-100" id="position" placeholder="find position" /> </div> <div class="col-2"> <div class="d-flex align-items-end justify-content-end h-100"> <button class="btn ti-close remove-team-member1" data-toggle="tooltip" data-placement="top" title="Remove Team Member"></button> </div> </div> </div> </div>';
 
-    $(document).on('click', '.add-team-member1', function() {
+    $(document).on('click', '.add-team-member1', function () {
         $('.team-members').append(teamMember1);
     });
-    $(document).on('click', '.remove-team-member1', function() {
+    $(document).on('click', '.remove-team-member1', function () {
         $(this).closest($('.vct-team-member-block')).slideUp(200);
-        setTimeout(function() {
+        setTimeout(function () {
             $(this).closest($('.vct-team-member-block')).remove();
         }, 150);
     });
@@ -150,7 +150,7 @@ $(function() {
     var saveBtn = $('.save-new-team');
     var teamMemberBlock = $('.vct-team-member-block');
     var deleteTeam = $('.delete-team');
-    trigger.on('click', function() {
+    trigger.on('click', function () {
         teamSelect.hide();
         newTeam.show().focus();
         inputVal.val('');
@@ -163,7 +163,7 @@ $(function() {
 
     //enable save button
     var saveBtn = $('.save-new-team');
-    $('input.vct-new-team').on('keyup', function() {
+    $('input.vct-new-team').on('keyup', function () {
         if ($(this).val() != "") {
             saveBtn.prop('disabled', false);
         } else {
