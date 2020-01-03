28ec47ea41b9d316ee7868a0de31749f155028a2
Oggy <ognjen.djokic@gmail.com>
2020-01-03 15:55:29 +0100

    Dashboard 2.0 update

--- assets/js/tmv20.js
+++ assets/js/tmv20.js
@@ -11,10 +11,13 @@ $('.time-module').on('click', function () {
 $('.time-module').click(function(event){
         var timeModule = $('.time-module');
         var moduleStatus = $('.module-status');
+        var moduleIcon = $('.module-icon');
         timeModule.removeClass('selected');
         timeModule.find(moduleStatus).removeClass('active');
+        timeModule.find(moduleIcon).removeClass('selected');
         $(this).addClass('selected');
         $(this).find(moduleStatus).addClass('active');
+        $(this).find(moduleIcon).addClass('selected');
         event.preventDefault();
     });
 
@@ -23,49 +26,49 @@ $('#days-spent').click(function () {
     var timeModule = $('.time-module');
     var panelContainer = $('.panel-container');
     panelContainer.hide();
-    $(this).parents('.left-items').find('#days-spent-data').show();
+    $(this).parents('.left-items').find('#days-spent-data').fadeIn();
 });
 
 $('#days-previous').click(function () {
     var timeModule = $('.time-module');
     var panelContainer = $('.panel-container');
     panelContainer.hide();
-    $(this).parents('.left-items').find('#days-previous-data').show();
+    $(this).parents('.left-items').find('#days-previous-data').fadeIn();
 });
 
 $('#days-free').click(function () {
     var timeModule = $('.time-module');
     var panelContainer = $('.panel-container');
     panelContainer.hide();
-    $(this).parents('.left-items').find('#days-free-data').show();
+    $(this).parents('.left-items').find('#days-free-data').fadeIn();
 });
 
 $('#days-earned').click(function () {
     var timeModule = $('.time-module');
     var panelContainer = $('.panel-container');
     panelContainer.hide();
-    $(this).parents('.left-items').find('#days-earned-data').show();
+    $(this).parents('.left-items').find('#days-earned-data').fadeIn();
 });
 
 $('#days-assigned').click(function () {
     var timeModule = $('.time-module');
     var panelContainer = $('.panel-container');
     panelContainer.hide();
-    $(this).parents('.left-items').find('#days-assigned-data').show();
+    $(this).parents('.left-items').find('#days-assigned-data').fadeIn();
 });
 
 $('#days-available').click(function () {
     var timeModule = $('.time-module');
     var panelContainer = $('.panel-container');
     panelContainer.hide();
-    $(this).parents('.left-items').find('#days-available-data').show();
+    $(this).parents('.left-items').find('#days-available-data').fadeIn();
 });
 
 $('#days-sickleave').click(function () {
     var timeModule = $('.time-module');
     var panelContainer = $('.panel-container');
     panelContainer.hide();
-    $(this).parents('.left-items').find('#days-sickleave-data').show();
+    $(this).parents('.left-items').find('#days-sickleave-data').fadeIn();
 });
 
 $('.js_book').on('click', function(){
