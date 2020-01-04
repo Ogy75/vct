eebc846a170054d8a38ebf55bedd73b00a423374
Oggy <ognjen.djokic@gmail.com>
2020-01-04 11:58:32 +0100

    time management page v2.0 update

--- assets/js/tmv20.js
+++ assets/js/tmv20.js
@@ -1,6 +1,6 @@
-$('.time-module').on('click', function () {
-  $(this).addClass('selected');
-  //$('.time-module').hasClass('selected').not($(this)).removeClass('selected');  
+$('.time-module').on('click', function() {
+    $(this).addClass('selected');
+    //$('.time-module').hasClass('selected').not($(this)).removeClass('selected');  
 });
 
 // $('.time-module').click(function() {
@@ -8,70 +8,70 @@ $('.time-module').on('click', function () {
 //     $(this).parents('.row').siblings().removeClass('selected');
 //   });
 
-$('.time-module').click(function(event){
-        var timeModule = $('.time-module');
-        var moduleStatus = $('.module-status');
-        var moduleIcon = $('.module-icon');
-        timeModule.removeClass('selected');
-        timeModule.find(moduleStatus).removeClass('active');
-        timeModule.find(moduleIcon).removeClass('selected');
-        $(this).addClass('selected');
-        $(this).find(moduleStatus).addClass('active');
-        $(this).find(moduleIcon).addClass('selected');
-        event.preventDefault();
-    });
+$('.time-module').click(function(event) {
+    var timeModule = $('.time-module');
+    var moduleStatus = $('.module-status');
+    var moduleIcon = $('.module-icon');
+    timeModule.removeClass('selected');
+    timeModule.find(moduleStatus).removeClass('active');
+    timeModule.find(moduleIcon).removeClass('selected');
+    $(this).addClass('selected');
+    $(this).find(moduleStatus).addClass('active');
+    $(this).find(moduleIcon).addClass('selected');
+    event.preventDefault();
+});
 
 
-$('#days-spent').click(function () {
+$('#days-spent').click(function() {
     var timeModule = $('.time-module');
     var panelContainer = $('.panel-container');
     panelContainer.hide();
     $(this).parents('.left-items').find('#days-spent-data').fadeIn();
 });
 
-$('#days-previous').click(function () {
+$('#days-previous').click(function() {
     var timeModule = $('.time-module');
     var panelContainer = $('.panel-container');
     panelContainer.hide();
     $(this).parents('.left-items').find('#days-previous-data').fadeIn();
 });
 
-$('#days-free').click(function () {
+$('#days-free').click(function() {
     var timeModule = $('.time-module');
     var panelContainer = $('.panel-container');
     panelContainer.hide();
     $(this).parents('.left-items').find('#days-free-data').fadeIn();
 });
 
-$('#days-earned').click(function () {
+$('#days-earned').click(function() {
     var timeModule = $('.time-module');
     var panelContainer = $('.panel-container');
     panelContainer.hide();
     $(this).parents('.left-items').find('#days-earned-data').fadeIn();
 });
 
-$('#days-assigned').click(function () {
+$('#days-assigned').click(function() {
     var timeModule = $('.time-module');
     var panelContainer = $('.panel-container');
     panelContainer.hide();
     $(this).parents('.left-items').find('#days-assigned-data').fadeIn();
 });
 
-$('#days-available').click(function () {
+$('#days-available').click(function() {
     var timeModule = $('.time-module');
     var panelContainer = $('.panel-container');
     panelContainer.hide();
     $(this).parents('.left-items').find('#days-available-data').fadeIn();
 });
 
-$('#days-sickleave').click(function () {
+$('#days-sickleave').click(function() {
     var timeModule = $('.time-module');
     var panelContainer = $('.panel-container');
     panelContainer.hide();
     $(this).parents('.left-items').find('#days-sickleave-data').fadeIn();
 });
 
-$('.js_book').on('click', function(){
+$('.js_book').on('click', function() {
     $('.js_bookingMessage').removeClass('d-none');
 });
 
@@ -81,4 +81,17 @@ var dd = String(today.getDate()).padStart(2, '0');
 var mm = String(today.getMonth() + 1).padStart(2, '0');
 var yyyy = today.getFullYear();
 today = dd + '.' + mm + '.' + yyyy;
-document.querySelector('.action-date').innerHTML = today;
+//document.querySelector('.action-date').innerHTML = today;
+
+//team toggle menu
+$('.team-name').on('click', function() {
+    $(this).parents('.vct-teams').find('.team-list').toggle();
+    $(this).toggleClass('visible');
+});
+
+//on site toggle
+$('.toggle-onsite').on('click', function(ev) {
+    var trigger = $(ev.target);
+    trigger.parents('.parent').find('.on-site-row').toggleClass('d-none');
+    trigger.toggleClass('fa-chevron-up');
+});
